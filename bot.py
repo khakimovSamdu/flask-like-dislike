24f72beb75357167b54b0c61949668263c08c4aa
khakimovSamdu <allamurodxakimov@gmail.com>
2024-02-03 02:35:44 +0500

    flask like dislike

--- bot.py
+++ bot.py
@@ -29,13 +29,21 @@ def start(user: str):
 app = Flask(__name__)
 TOKEN = "6984427979:AAGZ08mIUVwcSgiOzPpS7QZX31uP-_XWyE8"
 bot = Bot(TOKEN)
+url = "https://allamurodxakimov.pythonanywhere.com/"
 @app.route('/', methods=['POST'])
 def main():
     data = request.get_json()
     user = data['message']['from']
     print(data)
     if data['message'].get('text')!=None:
-        if data['message']['text']=="👍":
+        if data['message'].get('text')=='/start':
+            add_user(chat_id=str(user['id']))
+            bot.send_message(
+            chat_id = user['id'],
+            text=f'Hello, {user["first_name"]}! Press one of the buttons.',
+                reply_markup=keyboard
+            )
+        elif data['message']['text']=="👍":
             if not is_user(str(user['id'])):
                 start(user=data['message']['from'])
             inc_like(chat_id=str(user['id']))
@@ -66,12 +74,21 @@ def main():
                 text=f'🔥 likes: {user_data["likes"]}\n💣 dislikes: {user_data["dislikes"]}',
                 reply_markup=keyboard
             )
+        else:
+            bot.send_message(
+            chat_id = user['id'],
+            text=f'Please {user["first_name"]} send me the button. My function works based on the following buttons',
+                reply_markup=keyboard
+            )
     else:
         bot.send_message(
-                        chat_id = user['id'],
-                        text=f'Hello, {user["first_name"]}! Error one of the buttons.',
-                        reply_markup=keyboard
-                    )
+            chat_id = user['id'],
+            text=f'Please {user["first_name"]} send me the button. My function works based on the following buttons',
+                reply_markup=keyboard
+            )
     return "Hello programmer"
+bot.set_webhook(url)
+bot.get_webhook_info()
+
 if __name__== "__main__":
     app.run(debug=True)
